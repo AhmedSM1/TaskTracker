8ac02ca04ab5284b52fc37f1e05753c882c01a28
AhmedSM1 <ahmedalmansoer@gmail.com>
2021-11-18 02:07:07 +0300

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+         {{  title  }}
+    </header>
+     <Button text = "add" color = blue />
+     <Button text = "delete" color = red />
+</template>
+
+<script>
+import Button from "./Button.vue"
+export default {
+  name: "Header",
+  props: {
+      "title": String
+  },
+  components: {
+      Button,
+  }
+}
+
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
